ac7633ae7f8a55306c1582cb32b9f5780172ae92
Adrian06112006 <129766178+Adrian06112006@users.noreply.github.com>
2024-08-27 11:03:02 -0300

    eteeyytey

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-const caixaPrincipal = document.querySelector(".caixa-pricipal")
-const caixaPergunta = document.querySelector(".caixa-pergunta")
-const caixaAlternativas = document.querySelector(."caixa-alternativas")
-const caixaResultado = document.querySelector("caixa-resultado")
+const caixaPrincipal = document.querySelector(".caixa-pricipal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const testoResultado = document.querySelector(".texto-resultado");
 
+
+const Perguntas = [
+    {
+
+        enunciado: "Pergunta 1"
+        alternativas: ["alternativa1","alterntiva2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 2"
+        alternativas: ["alternativa1","alterntiva2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 3"
+        alternativas: ["alternativa1","alterntiva2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 4"
+        alternativas: ["alternativa1","alterntiva2","alternativa3"]
+    },
+
+];
